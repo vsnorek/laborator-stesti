60989353b8e9483da6a675ce1206b2c312bff0c5
vsnorek <vaclav.snorek@gmail.com>
2026-09-25 21:11:43 +0200

    chore: remove deprecated marketplace config

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,6 @@ export default defineConfig({
             plugins: [formsPlugin()],
             sandboxed: [webhookNotifier],
             sandboxRunner: sandbox(),
-            marketplace: "https://marketplace.emdashcms.com",
         }),
     ],
     fonts: [
